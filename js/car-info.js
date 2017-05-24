68b3ac423cdd7a5593530a185e60c0ae5db32d5f
lilingling <1192246772@qq.com>
2017-05-25 00:19:30 +0800

    'map'

--- js/car-info.js
+++ js/car-info.js
@@ -1,1 +1,27 @@
 
+    var map = new AMap.Map('mapContainer', {
+        // 设置中心点
+        center:[113.371,23.05],
+        // 设置缩放级别
+        zoom: 16
+    });
+    //地图中添加地图操作ToolBar插件
+    map.plugin(["AMap.ToolBar"],function() {
+        var toolBar = new AMap.ToolBar();
+        map.addControl(toolBar);
+    });
+    addMarker(113.371,23.05);
+
+
+function addMarker(lng,lat){
+    var markers = [];
+    marker = new AMap.Marker({
+        icon:"images/map-icon.png",
+        position:new AMap.LngLat(lng,lat)
+    });
+    marker.setMap(map);  //在地图上添加点
+    markers.push(marker);
+}
+
+
+
